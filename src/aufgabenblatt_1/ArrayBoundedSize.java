3a6ebb324dbc17e75e0fd11e45e484e35b44bdcb
Osarua <morro34@hotmail.de>
2016-03-23 14:47:11 +0100

    array bounded size, interface mehr kommentiert

--- src/aufgabenblatt_1/ArrayBoundedSize.java
+++ src/aufgabenblatt_1/ArrayBoundedSize.java
@@ -1,5 +1,161 @@
 package aufgabenblatt_1;
 
-public class ArrayBoundedSize {
+/**
+ * TI3 ADP, SS16 
+ * @author Julian
+ * Aufgabenblatt 1: Eine Liste umgesetzt mit einem Array fester Groesse.
+ * Wenn die Kapazitaet des Arrays nicht mehr ausreicht wird dieses vergroessert.
+ * @param <E> Elemente eines Typen 
+ */
+public class ArrayBoundedSize<E> implements Liste<E>{
 
+	/**
+	 * Liste(Array Bounded Size) von Elementen
+	 */
+	private Object[] liste;
+
+	/**
+	 * Die Anzahl der enhaltenen Elemente in der Liste
+	 */
+	private int anzahlDerElemente;
+
+	/**
+	 * construct: ANFANGSKAP X LIST -> LIST
+	 * Precondition: keine
+	 * Postcondition: Die Anzahl der Elemente in der Liste ist 0 und die Liste(Array)  
+	 * wird erzeugt mit einer Anfangskapizitaet. Wenn die anfangsKapazitaet <=0 wird eine
+	 * Exception geworfen.   
+	 * @param anfangsKapazitaet des Arrays
+	 */
+	public ArrayBoundedSize(int anfangsKapazitaet) {
+		anzahlDerElemente = 0;
+		try {
+			if (anfangsKapazitaet <= 0) {
+				throw new IllegalArgumentException("Ungueltige Anfangskapazitaet: " + anfangsKapazitaet);
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.print(e.getMessage());
+			e.printStackTrace();
+		}
+		liste = new Object[anfangsKapazitaet];
+	}
+
+	@Override
+	public void einfuegen(E element, int position) {
+		gueltigePosition(position);
+		elementMussUngleichNullSein(element);
+		if (anzahlDerElemente < liste.length) {
+			for (int i = (anzahlDerElemente - 1); i >= position; --i) {
+				liste[i + 1] = liste[i];
+			}
+		} else {
+			arrayVergroessern();
+			
+		}
+		liste[position] = element;
+		anzahlDerElemente++;
+	}
+
+	/**
+	 * plusCapacity: LIST X INT -> LIST
+	 * Precondition: keine
+	 * Postcondition: Array Liste ist um alte Kapazitaet * 3/2+1 vergroessert. 
+	 */
+	private void arrayVergroessern() {
+		Object[] listePlus;
+		listePlus = new Object[liste.length * 3 / 2 + 1];
+		for (int i = 0; i < liste.length; i++) {
+			listePlus[i] = liste[i];
+		}
+		liste = listePlus;
+	}
+	
+	@Override
+	public void entfernen(int position) {
+		gueltigePosition(position);
+		for (int i = position; i < (anzahlDerElemente - 1); i++) {
+			liste[i] = liste[i + 1];
+		}
+		liste[anzahlDerElemente - 1] = null;
+		anzahlDerElemente--;
+	}
+
+	@Override
+	public int finden(E element) {
+		elementMussUngleichNullSein(element);
+		for (int i = 0; i < anzahlDerElemente; i++) {
+			if (element.equals(liste[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public Object elementAnPosition(int position) {
+		gueltigePosition(position);
+		return liste[position];
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void listenZusammenfuegen(Liste<E> andereListe) {
+		try {
+			if (andereListe == null) {
+				throw new IllegalArgumentException("Liste darf nicht null sein");
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.print(e.getMessage());
+			e.printStackTrace();
+		}
+		if(andereListe instanceof ArrayBoundedSize<?>) {
+			for(int i=0;i<=andereListe.groesseDerListe();i++){
+				einfuegen((E) andereListe.elementAnPosition(i), anzahlDerElemente+i);
+			}
+		}
+		
+	}
+
+	@Override
+	public int groesseDerListe() {
+		return anzahlDerElemente;
+	}
+	
+	/**
+	 * notNull: LIST X ELEM -> ELEM 
+	 * Precondition: keine
+	 * Postcondition: Wirft eine Exception, fall das Element die Referenz null hat.
+	 * @param element welches ueberprueft werden soll
+	 */
+	private void elementMussUngleichNullSein(E element) {
+		try {
+			if (element == null) {
+				throw new IllegalArgumentException("Element darf nicht null sein");
+			}
+		} catch (IllegalArgumentException e) {
+			System.out.print(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+    /**
+     * truePosition: LIST X POS -> POS
+     * Precondition: keine
+     * Postcondition: Wirft eine Exception, wenn der Index zugriff mit dem 
+     * aktuellen Parameter ungueltig ist.
+     * Dies tritt ein, wenn die Position Position(Index) < 0 || Index > Kapazitaet 
+     * @param position welche ueberprueft werden soll
+     */
+	private void gueltigePosition(int position) {
+		try {
+			if ((position < 0) || (position > liste.length)) {
+				throw new IndexOutOfBoundsException("Ungueltiger Index zugriff: " + position);
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out.print(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
 }
+ 
--- src/aufgabenblatt_1/Liste.java
+++ src/aufgabenblatt_1/Liste.java
@@ -5,36 +5,86 @@ package aufgabenblatt_1;
  * @author Julian
  * Aufgabenblatt 1: Ein Interface für eine Liste mit Elementen.
  * Es gibt genau sechs Operationen(öffentliche Methoden) die auf einer Liste 
- * ausgeführt werden können. In den Methodenkommentar steht die jeweilige Operation
+ * ausgefuehrt werden koennen. Objektmengen der Funktionen: ELEM, LIST, POS.
+ * In den Methodenkommentar steht die jeweilige Operation
  * als Funktion. Dahinter Pre- und Postconditions sowie @param und @return.
- * @param <E> Elemente die verarbeitet werden
+ * In einer Liste kann ein Element mehrmals vorkommen. Innerhalb einer Liste muessen alle
+ * Elemente von dem gleichen Typ sein. 
+ * @param <E> Elemente eines Typen 
  */
 public interface Liste<E> {
 
 	/**
 	 * insert: LIST X POS X ELEM -> LIST
-	 * Precondition: Gültiger Index >=0
-	 * Postcondition: Element ist in der Liste enthalten oder es wird eine Exception 
-	 * geworfen, wenn das Element den Wert Null hat. Es wird auch 
-	 * eine Exception geworfen, falls das Element der Liste nicht hinzugefuegt werden kann. 
+	 * Precondition: keine
+	 * Postcondition: Das Element ist in der Liste enthalten. Wird das Element nicht 
+	 * am Ende der Liste eingefuegt muessen alle Elemente eine Position weiter nach hinten
+	 * ruecken. Wenn position gleich der Laenge der Liste ist, dann wird das Element am Ende 
+	 * der Liste eingefuegt. Wird die Methode mit einem Array Implementiert muss sich beim 
+	 * uerberschreiten der Kapazitaet, dass Array vergroessern. Die Anzahl der Elemente in der
+	 * Liste erhoeht sich um eins. Wenn das Element den Wert Null hat oder falls das Element 
+	 * der Liste nicht hinzugefuegt werden kann. Dann wird stattdessen eine Exception geworfen. 
+	 * Wenn die Position < 0 || Position > Kapazitaet wird auch eine Exeption geworfen. 
 	 * @param element Welches in die List eingefuegt werden soll 
-	 * @param position An dieser Position >= 0 soll das Element hinzugefuegt werden
+	 * @param position An dieser Position soll das Element hinzugefuegt werden
 	 */
- public void insert(E element, int position);
+	public void einfuegen(E element, int position);
  
- public E delete(int position);
+ 	/**
+	 * delete: LIST X POS -> LIST
+	 * Precondition: keine
+	 * Postcondition: Die Liste beinhaltet nicht mehr das Element an dieser Position(Index).
+	 * Wird das Element nicht am Ende der Liste Entfernt muessen alle Elemente um eins 
+	 * nach vorne verschoben werden. Dekrementierung(minus eins) der Anzahl der Elmente in
+	 * der Liste. Wird auf einen ungueltigen Index zugegriffen (Position < 0 || Position > Kapazitaet)
+	 * wird das Element nicht entfernt und eine Exception geworfen. 
+	 * Dies ist auch der fall, wenn an einem korrekten Index kein Element ist. 
+	 * @param position an dieser Position soll ein Element entfernt werden
+	 */
+	public void entfernen(int position);
  
 	/**
-	 * Precondition:
-	 * Postcondition:
-	 * @param element
-	 * @return 
+	 * find: LIST X ELEM -> POS
+	 * Precondition: keine
+	 * Postcondition: Das Element ist in der Liste und die Position(Index) wird
+	 * zurueckgegeben oder es wird nicht gefunden. Dann wird -1 zurueckgegeben.
+	 * Wenn, dass Element nicht kompatibel zu den Typen der Liste ist wird stattdessen eine
+	 * Exception geworfen. Dies passiert auch, wenn das Element Null ist.
+	 * @param element nach dem die Liste durchsucht werden soll
+	 * @return int Position des gefundenen Element oder -1 fuer nicht gefunden
 	 */
- public int find(E element);
- 
- public E retrieve(int poition);
- 
- public E concat(E liste);
- 
- public int size();
+	public int finden(E element);
+
+	/**
+	 * retrieve: LIST X POS -> ELEM
+	 * Precondition: keine
+	 * Postcondition: Es wird das Element an der uebergebenen Position zurueckgegeben.
+	 * Falls der aktuelle Parameter position (Position < 0 || Position > Kapazitaet) 
+	 * ist wird eine Exception geworfen. Die Methode wird auch ausgefuehr, wenn das Element 
+	 * den Wert null hat an dieser Position.
+	 * @param position des Element in der Liste
+	 * @return Object Element an dieser Position 
+	 */
+	public Object elementAnPosition(int position);
+
+	/**
+	 * concat: LIST X LIST -> LIST
+	 * Precondition: keine 
+	 * Postcondition: andereListe muss das Interface Liste<E> implementieren und mit den konkreten 
+	 * Klassentyp(auch gnerischer Typ) dieser Liste kompatibel sein. Ansonsten wird eine Exception geworfen.
+	 * Wenn die beiden Listen Typen kompatibel sind, fuegt die Methode diese zusammen. Dabei erhoeht sich die
+	 * Anzahl der Elemente um die anzahl der neuen Elmenten. Die anzufuegende Liste wird am Ende dieser Liste
+	 * "angehaengt". Wird die Lise mit einem Array Implementiert, vergoessert sich ggf. dieses.
+	 * @param liste Die mit dieser zusammengefuegt werden soll
+	 */
+	public void listenZusammenfuegen(Liste<E> andereListe);
+
+	/**
+	 * size: LIST -> INT
+	 * Precondition: keine 
+	 * Postcondition: Methode gibt die Anzahl der Elemente in der Liste zurueck. 
+	 * @return int, die Anzahl der Elemente in der Liste
+	 */
+	public int groesseDerListe();
+	
 }
