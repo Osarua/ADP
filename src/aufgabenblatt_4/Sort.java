400918be128dbdb4f6b0a3406c85d5f021761bce
Osarua <morro34@hotmail.de>
2016-04-24 20:32:13 +0200

    sortK

--- src/aufgabenblatt_4/Sort.java
+++ src/aufgabenblatt_4/Sort.java
@@ -3,29 +3,46 @@ package aufgabenblatt_4;
 
 public class Sort {
 	
-		private LinkList[] sortierListe;
+		private Object[] sortierListe;
 		private int exponent;
+		private final int feldTeiler=10;
 		
 		public Sort (int exponentPar) {
 			exponent=exponentPar;
 			sortierListe=arrayErstellen();
 		}
 		
-		private LinkList[] arrayErstellen () {
-			int anzahl = (int) Math.pow(10, exponent)/10;
-			LinkList [] array = new LinkList [anzahl];
+		private Object[] arrayErstellen () {
+			int anzahl = ((int) Math.pow(10, exponent)*100)/feldTeiler+1;
+			Object [] array = new Object [anzahl];
 			return array;
 		}
-		private void arrayAufbauen () {
+		/**
+		 * Bei vor Wiederverwenderung des Objektes benutzen um das Array zu leeren
+		 */
+		public void arrayLeeren () {
 			for (int i=0;i<sortierListe.length;i++) {
-				sortierListe[i]=new LinkList();
+				sortierListe[i]=null;
 			}
 		}
 		private void elementEinfuegen (Object element) {
 			int index=element.hashCode();
-			index=index-(700*(int)Math.pow(10, exponent));
-			index=index/1000;
-			sortierListe[index].addObject(element);
+			index=(index-(700*(int)Math.pow(10, exponent)))/feldTeiler;
+			if (sortierListe[index]!=null) {
+				if (sortierListe[index] instanceof LinkList) {
+					LinkList lokaleListe=(LinkList) sortierListe[index];
+					lokaleListe.addObject(element);
+				}
+				else {
+					LinkList lokaleListe = new LinkList ();
+					lokaleListe.addObject(sortierListe[index]);
+					lokaleListe.addObject(element);
+					sortierListe[index]=lokaleListe;
+				}
+			}
+			else {
+				sortierListe[index]=element;
+			}
 		}
 		/**
 		 * Sortiert die übergebene Liste.
@@ -35,19 +52,31 @@ public class Sort {
 		 * @return list
 		 */
 		public Object [] sortier (Object[] list) {
-			arrayAufbauen();
 			for (int i=0;i<list.length;i++) {
 				elementEinfuegen (list[i]);
 			}
 			int index=0;
 			for (int i=0;i<sortierListe.length;i++) {
-				for (int j=0;j<sortierListe[i].getAnzahl();j++) {
-					list[index]=sortierListe[i].getObjectAtPos();
-					sortierListe[i].erhoehePos();
-					index++;
+				if (sortierListe[i]!=null) {
+					if (sortierListe[i] instanceof LinkList) {
+						LinkList lokaleListe = (LinkList) sortierListe[i];
+						for (int j=0;j<lokaleListe.getAnzahl();j++) {
+							list[index]=lokaleListe.getObjectAtPos();
+							lokaleListe.erhoehePos();
+							index++;
+						}
+					}
+					else {
+						list[index]=sortierListe[i];
+						index++;
+					}
+				}
+				if (index==list.length) {
+					break;
 				}
 			}
 			return list;
 		}
+
 	}
 
--- src/aufgabenblatt_4/SortTest.java
+++ src/aufgabenblatt_4/SortTest.java
@@ -21,6 +21,137 @@ import static org.junit.Assert.assertTrue;
 		public int erstelleSchluessel(int n) {
 			return (int) (Math.random() * (n*100)) + (700 * n);
 		}
+		
+		/**
+		 * Untersuchung mit k = 1
+		 */
+		@Test
+		public void testSchnellesSortierenNk1() {
+			int n = 10;
+			Integer[] feldI = new Integer[n];
+			for (int i = 0; i < n; i++) {
+				feldI[i] = erstelleSchluessel(n);
+			}
+			Sort sort = new Sort (1);
+			long zeit= System.currentTimeMillis();
+			sort.sortier(feldI);
+			zeit = System.currentTimeMillis() - zeit;
+			System.out.println(sort);
+			System.out.println("N: " + n + ", Laufzeit: " + zeit + "\n");
+
+			boolean korrektSortiert = true;
+			for (int i = 0; i < n - 1; i++) {
+				if (feldI[i].hashCode() > feldI[i + 1].hashCode()) {
+					korrektSortiert = false;
+				}
+			}
+			assertEquals("Sollte true sein, wenn Array korrekt sortiert ist", korrektSortiert, true);
+		}
+
+		/**
+		 * Untersuchung mit k = 2
+		 */
+		@Test
+		public void testSchnellesSortierenNk2() {
+			int n = 100;
+			Integer[] feldI = new Integer[n];
+			for (int i = 0; i < n; i++) {
+				feldI[i] = erstelleSchluessel(n);
+			}
+			Sort sort = new Sort (2);
+			long zeit= System.currentTimeMillis();
+			sort.sortier(feldI);
+			zeit = System.currentTimeMillis() - zeit;
+			System.out.println(sort);
+			System.out.println("N: " + n + ", Laufzeit: " + zeit + "\n");
+
+			boolean korrektSortiert = true;
+			for (int i = 0; i < n - 1; i++) {
+				if (feldI[i].hashCode() > feldI[i + 1].hashCode()) {
+					korrektSortiert = false;
+				}
+			}
+			assertEquals("Sollte true sein, wenn Array korrekt sortiert ist", korrektSortiert, true);
+		}
+
+
+	/**
+	 * Untersuchung mit k = 3
+	 */
+	@Test
+	public void testSchnellesSortierenNk3() {
+		int n = 1000;
+		Integer[] feldI = new Integer[n];
+		for (int i = 0; i < n; i++) {
+			feldI[i] = erstelleSchluessel(n);
+		}
+		Sort sort = new Sort (3);
+		long zeit= System.currentTimeMillis();
+		sort.sortier(feldI);
+		zeit = System.currentTimeMillis() - zeit;
+		System.out.println(sort);
+		System.out.println("N: " + n + ", Laufzeit: " + zeit + "\n");
+
+		boolean korrektSortiert = true;
+		for (int i = 0; i < n - 1; i++) {
+			if (feldI[i].hashCode() > feldI[i + 1].hashCode()) {
+				korrektSortiert = false;
+			}
+		}
+		assertEquals("Sollte true sein, wenn Array korrekt sortiert ist", korrektSortiert, true);
+	}
+		/**
+		 * Untersuchung mit k = 4
+		 */
+		@Test
+		public void testSchnellesSortierenNk4() {
+			int n = 10000;
+			Integer[] feldI = new Integer[n];
+			for (int i = 0; i < n; i++) {
+				feldI[i] = erstelleSchluessel(n);
+			}
+			Sort sort = new Sort (4);
+			long zeit= System.currentTimeMillis();
+			sort.sortier(feldI);
+			zeit = System.currentTimeMillis() - zeit;
+			System.out.println(sort);
+			System.out.println("N: " + n + ", Laufzeit: " + zeit + "\n");
+
+			boolean korrektSortiert = true;
+			for (int i = 0; i < n - 1; i++) {
+				if (feldI[i].hashCode() > feldI[i + 1].hashCode()) {
+					korrektSortiert = false;
+				}
+			}
+			assertEquals("Sollte true sein, wenn Array korrekt sortiert ist", korrektSortiert, true);
+		}
+
+		/**
+		 * Untersuchung mit k = 5
+		 */
+		@Test
+		public void testSchnellesSortierenNk5() {
+			int n = 100000;
+			Integer[] feldI = new Integer[n];
+			for (int i = 0; i < n; i++) {
+				feldI[i] = erstelleSchluessel(n);
+			}
+			Sort sort = new Sort (5);
+			long zeit= System.currentTimeMillis();
+			sort.sortier(feldI);
+			zeit = System.currentTimeMillis() - zeit;
+			System.out.println(sort);
+			System.out.println("N: " + n + ", Laufzeit: " + zeit + "\n");
+
+			boolean korrektSortiert = true;
+			for (int i = 0; i < n - 1; i++) {
+				if (feldI[i].hashCode() > feldI[i + 1].hashCode()) {
+					korrektSortiert = false;
+				}
+			}
+			assertEquals("Sollte true sein, wenn Array korrekt sortiert ist", korrektSortiert, true);
+		}
+		
 		/**
 		 * Untersuchung mit k = 6
 		 */
@@ -32,7 +163,6 @@ import static org.junit.Assert.assertTrue;
 				feldI[i]= erstelleSchluessel(n);
 			}
 			Sort sort = new Sort (6);
-			sort.sortier(feldI);
 			long zeit= System.currentTimeMillis();
 			sort.sortier(feldI);
 			zeit = System.currentTimeMillis() - zeit;
